167386d37b37c4117e25508edc862f0f8cbfb49c
Wutengwei <15239184251@126.com>
2017-09-08 13:00:22 +0800

    v0.1.2

--- Example/Pods/Target Support Files/RCTools/RCTools-umbrella.h
+++ Example/Pods/Target Support Files/RCTools/RCTools-umbrella.h
@@ -10,8 +10,30 @@
 #endif
 #endif
 
+#import "FZButton.h"
+#import "FZDIYHeader.h"
+#import "FZRefreshFooter.h"
+#import "FZSwitch.h"
+#import "FZTextField.h"
+#import "LLAttributeLabel.h"
+#import "NSAttributedString+Extension.h"
+#import "NSDate+Extension.h"
+#import "NSDictionary+FZParametersSign.h"
+#import "NSDictionary+log.h"
 #import "NSObject+instance.h"
+#import "NSString+Extension.h"
+#import "UIBarButtonItem+FZBarButtonItem.h"
+#import "UICollectionView+FZCategory.h"
+#import "UIColor+Extension.h"
+#import "UIImage+FZCategory.h"
+#import "UIImageView+Extension.h"
+#import "UINavigationBar+Awesome.h"
+#import "UINavigationBar+Extension.h"
+#import "UITabBar+FZBage.h"
+#import "UIView+CornerRadius.h"
 #import "UIView+Extension.h"
+#import "UIViewController+FZHUD.h"
+#import "UIViewController+FZNavigationBar.h"
 
 FOUNDATION_EXPORT double RCToolsVersionNumber;
 FOUNDATION_EXPORT const unsigned char RCToolsVersionString[];
